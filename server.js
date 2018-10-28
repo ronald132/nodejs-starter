1732ba88f0121d6e58aed473e992fc4e37e887fa
ronald132 <ronald132@gmail.com>
2018-10-28 23:01:04 +0800

    initial project nodejs material

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejs_mate = require('ejs-mate');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('ejs', ejs_mate);
+app.set('view engine', 'ejs');
+
+app.use(require('./routes/main'));
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function(){
+  console.log("listening on port 3000");
+});
